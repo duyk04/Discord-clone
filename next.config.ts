9e8698862c237c1398cb7281d77d95f7896686df
Khuong Duy <140906531+duyk04@users.noreply.github.com>
2024-11-20 00:01:11 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: [
-      "uploadthing.com",
-      "utfs.io"
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "uploadthing.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        pathname: "/**",
+      },
     ],
   },
 };
