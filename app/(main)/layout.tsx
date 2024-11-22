560d0b626882ca342f3c5b1d7403728d5b780047
Khuong Duy <140906531+duyk04@users.noreply.github.com>
2024-11-22 16:49:07 +0700

    fix ui

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -3,10 +3,10 @@ import { NavigationSidebar } from "@/components/navigation/navigation-sidebar";
 const MainLayout = async ({ children }: { children: React.ReactNode }) => {
     return (  
         <div className="h-full">
-            <div className="md:flex h-full w-[72px]
+            <div className="hidden md:flex h-full w-[72px]
             z-30 flex-col fixed inset-y-0">
-            {/* <div className="opacity-0 md:opacity-100 h-full w-[72px]
-                                z-30 flex-col fixed inset-y-0"> */}
+            {/* <div className="invisible md:visible md:flex h-full w-[72px]
+            z-30 flex-col fixed inset-y-0"> */}
                 <div>
                     <NavigationSidebar/>
                 </div>
