2bff1bc8363ecfb8b7135a1be51a56cb5f214b0b
Khuong Duy <140906531+duyk04@users.noreply.github.com>
2024-11-22 16:49:18 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import type { Metadata } from "next";
 import "./globals.css";
+import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import { ClerkProvider } from '@clerk/nextjs'
 
